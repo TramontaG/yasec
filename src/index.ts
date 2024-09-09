30a5bb6c622c5286fc5b72dbb767b343acbe77b2
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2024-09-09 13:29:14 -0300

    making sure lib is correct

--- src/index.ts
+++ src/index.ts
@@ -37,31 +37,3 @@ export const createTaskQueue = () => {
 };
 
 module.exports = createTaskQueue;
-
-const taskQueue = createTaskQueue();
-
-// simple tasks for demonstration
-const task1 = () => {
-	return new Promise(resolve => {
-		console.log('started execution of task1');
-		setTimeout(() => resolve('task1 completed'), 1500);
-	});
-};
-
-const task2 = () => {
-	return new Promise(resolve => {
-		console.log('started execution of task2');
-
-		setTimeout(() => resolve('task2 completed'), 1000);
-	});
-};
-
-// Normal flow;
-console.log('NORMAL FLOW');
-// task1().then(console.log);
-// task2().then(console.log);
-
-// With task queue
-// console.log('QUEUED FLOW');
-taskQueue.awaitExecution(task1).then(console.log);
-taskQueue.awaitExecution(task2).then(console.log);
